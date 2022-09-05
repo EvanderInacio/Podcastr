ca711af0a60dc843099871573d918bf3c1470900
Evander Inacio <evander.20112@hotmail.com>
2022-09-04 22:04:27 -0300

    Json-server :alien:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
-import React from "react"
 
 
-export default function Home() {
+
+export default function Home(props) {
   return (
     <img src="/index.png" alt="" />
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8 // A cada 8 horas, quando alguém acessar essa pagina ele ira fazer uma nova chamada a API 
+  }
 }
