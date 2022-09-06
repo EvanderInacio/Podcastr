0d57e924b0698db96bcd5f9a2cc28ada888e82c2
Evander Inacio <evander.20112@hotmail.com>
2022-09-05 23:52:52 -0300

    Axios/Formatted data :package:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,59 @@
+import { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from '../services/api'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeSting'
+
+type Episode = {
+  id: string
+  title: string
+  members: string
+  published_at: string
+}
+
+type HomeProps = {
+  episodes: Episode[]
+}
+ 
+ 
+export default function Home(props: HomeProps) {
 
 
-
-export default function Home(props) {
+  
   return (
-    <img src="/index.png" alt="" />
+    <div>
+      <img src="/index.png" alt="" />
+      {/* <p>{JSON.stringify(props.episodes)}</p> */}
+    </div>
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8 // A cada 8 horas, quando alguém acessar essa pagina ele ira fazer uma nova chamada a API 
   }
